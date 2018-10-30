da49f82d89ea588da1cb6b6e7c5cbc52b92ff7ad
nikolai <nikolai.solo.b@gmail.com>
2018-10-30 17:14:32 -0500

    entered encoder info

--- FIRSTTestProject/src/main/java/frc/robot/subsystems/Zoomy.java
+++ FIRSTTestProject/src/main/java/frc/robot/subsystems/Zoomy.java
@@ -9,7 +9,7 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Victor;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import frc.robot.commands.Zippy;
@@ -18,10 +18,10 @@ import frc.robot.commands.Zippy;
  * An example subsystem.  You can replace me with your own Subsystem.
  */
 public class Zoomy extends Subsystem {
-public WPI_TalonSRX can0, can2, can1;
-public Victor vichtor;
-
-public MecanumDrive drive1;
+    public WPI_TalonSRX can0, can2, can1;
+    public Victor vichtor;
+    public Encoder lwheel, rwheel;
+    public MecanumDrive drive1;
 
 
 
@@ -38,7 +38,20 @@ public MecanumDrive drive1;
         // Max Velocity = 6.54
         // Max Acceleration
         // Max Jerk
-    
+        lwheel = new Encoder(3, 2, false, Encoder.Encodingtype.k4X);
+        rwheel = new Encoder(1, 0, false, Encoder.Encodingtype.k4X);
+
+        lwheel.setMaxPeriod(.1);
+		lwheel.setMinRate(10);
+		lwheel.setDistancePerPulse(1/1440);
+		lwheel.setReverseDirection(false);
+        lwheel.setSamplesToAverage(7);
+
+        rwheel.setMaxPeriod(.1);
+		rwheel.setMinRate(10);
+		rwheel.setDistancePerPulse(1/1440);
+		rwheel.setReverseDirection(false);
+		rwheel.setSamplesToAverage(7);
 
 
     }
