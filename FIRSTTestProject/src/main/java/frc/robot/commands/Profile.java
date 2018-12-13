0b0dbf125560521d1372cc31ffde96a167963846
nikolai <nikolai.solo.b@gmail.com>
2018-12-13 17:38:18 -0600

    setting up encoder follower command.

---
+++ FIRSTTestProject/src/main/java/frc/robot/commands/Profile.java
@@ -0,0 +1,58 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+import jaci.pathfinder.followers.*;
+import jaci.pathfinder.Pathfinder;
+import jaci.pathfinder.Trajectory;
+import jaci.pathfinder.Waypoint;
+import jaci.pathfinder.modifiers.TankModifier;
+
+public class Profile extends Command {
+    Trajectory left, right;
+public EncoderFollower follow = new EncoderFollower();
+
+  public Profile() {
+      requires(Robot.zoomy);
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  public void initialize() {
+      follow.configureEncoder(0,1440,15.24);
+      follow.configurePIDVA(0.8, 0, 0.8, (1/6.54), 0);
+
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return true; //follow.segment >= trajectory.length();
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
+}
