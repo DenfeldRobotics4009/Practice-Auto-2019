1cecb759f6c49e2490628fb3fadb5824390f64c3
Ian Walker <iwalkblood@gmail.com>
2018-12-20 17:53:33 -0600

    This is going to be a mess for a bit.

--- FIRSTTestProject/src/main/java/frc/robot/PathGeneration/Generator.java
+++ FIRSTTestProject/src/main/java/frc/robot/PathGeneration/Generator.java
@@ -5,7 +5,7 @@ import jaci.pathfinder.Waypoint;
 import jaci.pathfinder.modifiers.TankModifier;
 
 public class Generator {
-
+  
     public Generator(){
     }
         public static void main (String[] args){
--- FIRSTTestProject/src/main/java/frc/robot/commands/Profile.java
+++ FIRSTTestProject/src/main/java/frc/robot/commands/Profile.java
@@ -9,18 +9,19 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
+import frc.robot.PathGeneration.Generator;
 import jaci.pathfinder.followers.*;
-import jaci.pathfinder.Pathfinder;
 import jaci.pathfinder.Trajectory;
-import jaci.pathfinder.Waypoint;
-import jaci.pathfinder.modifiers.TankModifier;
+
 
 public class Profile extends Command {
-    Trajectory left, right;
-public EncoderFollower follow = new EncoderFollower();
+  Trajectory left;
+  public Generator generator = new Generator();
+  public EncoderFollower follow = new EncoderFollower();
 
   public Profile() {
       requires(Robot.zoomy);
+      requires(Robot.driveencoders);
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
 
@@ -31,6 +32,7 @@ public EncoderFollower follow = new EncoderFollower();
   public void initialize() {
       follow.configureEncoder(0,1440,15.24);
       follow.configurePIDVA(0.8, 0, 0.8, (1/6.54), 0);
+      
 
   }
 
@@ -42,7 +44,7 @@ public EncoderFollower follow = new EncoderFollower();
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return follow.segment >= trajectory.length();
+    return true; //follow.segment >= trajectory.length();
   }
 
   // Called once after isFinished returns true
